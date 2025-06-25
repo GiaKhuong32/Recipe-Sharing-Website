23c4f7057e88d4a9688362ba755420ea6869ddc1
phuc.nm01953 <phuc.nm01953@sinhvien.hoasen.edu.vn>
2025-06-25 15:25:23 +0700

    * fix cant run due to lower react version

--- recipe-sharing-site/src/index.js
+++ recipe-sharing-site/src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';  // <-- đúng module cho React 18
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
